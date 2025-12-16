ca09241bf050c437c6130d981575be00171a469a
itc-n24012 <n24012@std.it-college.ac.jp>
2025-12-16 16:33:41 +0900

    frループとインデックスの成績入力12/16

---
+++ Chapter4/score.py
@@ -0,0 +1,13 @@
+#学生の名前リスト
+students=['相沢いわし','伊藤うずら','上野えのき']
+
+#点数を格納するリスト
+scores=[0,0,0]
+
+#点数を入力するforループ
+for i in range(len(students)):
+    scores[i]=int(input(f"{students[i]}さんの点数を入力してください。 >> "))
+
+#結果を表示する
+for i in range(len(students)):
+    print(f"{students[i]}さん: {scores[i]}点")
