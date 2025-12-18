379cecae5ac4964fe5c6e4b4fd1a0994c800305d
itc-n24012 <n24012@std.it-college.ac.jp>
2025-12-18 13:57:50 +0900

    トランプ12/17

--- Chapter5/cards.py
+++ Chapter5/cards.py
@@ -1,10 +1,27 @@
 import random
 
-cards = []
-for i in range(1,53):
-    cards.append(i)
+cards = list(range(1, 53))
 
-print(cards) #シャッフル前
+print(cards)  # シャッフル前
 random.shuffle(cards)
-print(cards) #シャッフル後
+print(cards)  # シャッフル後
 
+pick = int(input("何枚目のカードを引きますか(1-52) >>"))
+card = cards[pick - 1]  # インデックスは0から
+
+# スート判定
+suits = ["スペード", "ハート", "ダイヤ", "クラブ"]
+suit = suits[(card - 1) // 13]
+
+# ナンバー判定
+number = (card - 1) % 13 + 1
+if number == 1:
+    number = "A"
+elif number == 11:
+    number = "J"
+elif number == 12:
+    number = "Q"
+elif number == 13:
+    number = "K"
+
+print(f"あなたが引いたカードは {suit} の {number} です")
